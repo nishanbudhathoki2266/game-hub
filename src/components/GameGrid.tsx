10ac0c09468c3c1845b2f1dffdc3c338648ed46c
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-22 13:14:05 +0545

    Added a genre fetching and refactored the custom hooks

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 
 const GameGrid = () => {
-    const { games, error, isLoading } = useGames();
+    const { data, error, isLoading } = useGames();
     const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
@@ -15,7 +15,7 @@ const GameGrid = () => {
             }
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding='10px'>
                 {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
-                {games.map(game => (
+                {data.map(game => (
                     <GameCard key={game.id} game={game} />
                 ))}
             </SimpleGrid>
