1c41a242baf6c552730196c825888e0283f242fc
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-22 14:20:38 +0545

    Added a missing card hover functionality

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -3,7 +3,11 @@ import { ReactNode } from "react"
 
 const CardContainer = ({ children }: { children: ReactNode }) => {
     return (
-        <Card borderRadius={10} overflow='hidden'>{children}</Card>
+        <Card borderRadius={10} overflow='hidden' _hover={{
+            cursor: 'pointer',
+            transform: 'scale(1.02)',
+            transition: 'transform ease .3s'
+        }}>{children}</Card>
     )
 }
 export default CardContainer
