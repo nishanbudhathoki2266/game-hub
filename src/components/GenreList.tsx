5893c374dd0f7163c48eb79b8e070688967ee807
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-22 13:36:50 +0545

    Added some loading and error states to genres fetching

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,14 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/img-url';
 
 const GenreList = () => {
-    const { data } = useGenres();
+    const { data, isLoading, error } = useGenres();
+
+    if (error) return <Text color='red'>Error fetching genres!</Text>;
+
+    if (isLoading) return <Spinner />
+
     return (
         <List>
             {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
