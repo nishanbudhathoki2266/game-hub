fe455c6288a9d25ccb21f598ec9b7dccbc607389
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-23 13:07:52 +0545

    Enhanced some sidebar's (genres') styling

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/img-url';
 
@@ -15,16 +15,19 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     if (isLoading) return <Spinner />
 
     return (
-        <List>
-            {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
-                <HStack>
-                    <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                    <Button fontSize='lg' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} color={genre.id === selectedGenre?.id ? '#ffa31a' : ''} variant='link' onClick={() => onSelectGenre(genre)}>
-                        {genre.name}
-                    </Button>
-                </HStack>
-            </ListItem>)}
-        </List>
+        <>
+            <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+            <List>
+                {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image boxSize='32px' objectFit='cover' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                        <Button whiteSpace='normal' textAlign='left' fontSize='lg' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} color={genre.id === selectedGenre?.id ? '#ffa31a' : ''} variant='link' onClick={() => onSelectGenre(genre)}>
+                            {genre.name}
+                        </Button>
+                    </HStack>
+                </ListItem>)}
+            </List>
+        </>
     )
 }
 export default GenreList;
