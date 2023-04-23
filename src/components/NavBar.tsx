7f484f0ac016109823c02ae76256627192708606
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-23 12:19:44 +0545

    Added a search component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react"
 import gamehub from './../assets/gamehub.png'
 import ColorModeSwitch from "./ColorModeSwitch"
+import SearchInput from "./SearchInput"
 
 const NavBar = () => {
     return (
-        < HStack justifyContent="space-between" padding='10px'>
+        < HStack padding='10px'>
             <Image src={gamehub} boxSize='100px' objectFit='contain' />
+            <SearchInput />
             <ColorModeSwitch />
         </ HStack>
     )
